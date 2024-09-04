29c1fb9879d72ef2d33466bc655494d87aa033ab
jai-chawla <jaichawla074@gmail.com>
2024-09-04 11:11:12 +0530

    live push

--- client/src/components/common/server_url.js
+++ client/src/components/common/server_url.js
@@ -1,4 +1,4 @@
-// const api='https://clone-internshub-api.vercel.app' 
-const api='http://localhost:4000'
+const api='https://clone-internshub-api.vercel.app' 
+// const api='http://localhost:4000'
 
 export default api;
